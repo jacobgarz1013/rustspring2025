3f0e5874f3e05ae5830a9698d3a356361094aad8
jacobgarz1013 <jacobgarz1013@gmail.com>
2025-02-28 10:08:19 -0600

    attendance verification

---
+++ ReadFromFile/target/debug/ReadFromFile.d
@@ -0,0 +1,1 @@
+C:\Users\drago\OneDrive\Documents\Rust\rustspring2025\ReadFromFile\target\debug\ReadFromFile.exe: C:\Users\drago\OneDrive\Documents\Rust\rustspring2025\ReadFromFile\src\main.rs
---
+++ ReadFromFile/target/debug/deps/ReadFromFile.d
@@ -0,0 +1,5 @@
+C:\Users\drago\OneDrive\Documents\Rust\rustspring2025\ReadFromFile\target\debug\deps\ReadFromFile.exe: src\main.rs
+
+C:\Users\drago\OneDrive\Documents\Rust\rustspring2025\ReadFromFile\target\debug\deps\ReadFromFile.d: src\main.rs
+
+src\main.rs:
